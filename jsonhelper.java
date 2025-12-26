d64b939dc986df59d14d7649c1a25de4bd0d6032
sunnypipo <dazi0130519@gmail.com>
2025-12-27 01:49:36 +0800

    noob move files move again

--- jsonhelper.java
+++
@@ -1,9 +0,0 @@
-public class jsonhelper {
-    public int number(String input){
-        return Integer.parseInt(input.split(":")[1].replace(",", "").trim());
-    }
-
-    public String text(String input){
-        return input.split(":")[1].replace("\"", "").replace(",", "").trim();
-    }
-}
